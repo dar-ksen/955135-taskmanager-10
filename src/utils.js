127d200b07ec1ab59226cedbcd8528d372d4cc4d
Dar <ksen.vl.set@gmail.com>
2019-12-05 09:56:03 +0300

    Divided the total export into several lines

--- src/utils.js
+++ src/utils.js
@@ -6,7 +6,7 @@ const formatterOptionsTime = {
 
 const TimeFormatter = new Intl.DateTimeFormat(`en-US`, formatterOptionsTime);
 
-export const formatTime = (date) => TimeFormatter.format(date);
+const formatTime = (date) => TimeFormatter.format(date);
 
 const formatterOptionsDate = {
   day: `numeric`,
@@ -15,24 +15,23 @@ const formatterOptionsDate = {
 
 const DateFormatter = new Intl.DateTimeFormat(`en-GB`, formatterOptionsDate);
 
-export const formatDate = (date) => DateFormatter.format(date);
+const formatDate = (date) => DateFormatter.format(date);
+const isFirst = (index) => index === 0;
+const take = (array, count, startPos = 0) => array.slice(startPos, startPos + count);
 
-export const isFirst = (index) => index === 0;
-export const take = (array, count, startPos = 0) => array.slice(startPos, startPos + count);
-
-export const RenderPosition = {
+const RenderPosition = {
   AFTERBEGIN: `afterbegin`,
   BEFOREEND: `beforeend`
 };
 
-export const createElement = (template) => {
+const createElement = (template) => {
   const newElement = document.createElement(`div`);
   newElement.innerHTML = template;
 
   return newElement.firstElementChild;
 };
 
-export const render = (container, element, place) => {
+const render = (container, element, place) => {
   switch (place) {
     case RenderPosition.AFTERBEGIN:
       container.prepend(element);
@@ -42,3 +41,6 @@ export const render = (container, element, place) => {
       break;
   }
 };
+
+export { formatTime, formatDate, isFirst, take,
+  RenderPosition, createElement, render };
