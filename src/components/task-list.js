e93e33f14a6bf2076546e58168804450f17daf17
Dar <ksen.vl.set@gmail.com>
2019-12-06 23:12:45 +0300

    Add task-list component

---
+++ src/components/task-list.js
@@ -0,0 +1,30 @@
+import { createElement } from '../utils';
+
+const getTaskListTemplate = () => {
+  return (`
+    <div class="board__tasks js-board__tasks"></div>
+  `);
+};
+
+export default class TaskList {
+  constructor() {
+    this._element = null;
+  }
+
+  getTemplate() {
+    return getTaskListTemplate();
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+
+}
