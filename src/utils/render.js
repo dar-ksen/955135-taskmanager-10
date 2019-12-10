e0902b80b9d5358852abfa64ec5103d57a43c6ff
Dar <ksen.vl.set@gmail.com>
2019-12-10 16:41:27 +0300

    Separate utils.js into two files: render.js and common.js

---
+++ src/utils/render.js
@@ -0,0 +1,42 @@
+const RenderPosition = {
+  AFTER_BEGIN: `afterbegin`,
+  BEFORE_END: `beforeend`
+};
+
+const createElement = (template) => {
+  const newElement = document.createElement(`div`);
+  newElement.innerHTML = template;
+
+  return newElement.firstElementChild;
+};
+
+const renderComponent = (container, component, place) => {
+  const element = component.getElement();
+  switch (place) {
+    case RenderPosition.AFTER_BEGIN:
+      container.prepend(element);
+      break;
+    case RenderPosition.BEFORE_END:
+      container.append(element);
+      break;
+  }
+};
+
+const replace = (newComponent, oldComponent) => {
+  const parentElement = oldComponent.getElement().parentElement;
+  const newElement = newComponent.getElement();
+  const oldElement = oldComponent.getElement();
+
+  const isExistElements = Boolean(parentElement && newElement && oldElement);
+
+  if (isExistElements && parentElement.contains(oldElement)) {
+    parentElement.replaceChild(newElement, oldElement);
+  }
+};
+
+export {
+  RenderPosition,
+  createElement,
+  renderComponent,
+  replace
+};
