7d3cf5301748f205ff917cc7a9e462fdd9b9e7e5
Dar <ksen.vl.set@gmail.com>
2019-12-23 09:11:35 +0300

    Connect the model to the main

--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
 import MenuComponent from './components/menu';
 import FilterComponent from './components/filter';
 
+import TasksModel from './models/tasks';
+
 import BoardComponent from './components/board';
 import BoardController from './controllers/board';
 
 import { tasks } from './mock/task.js';
 import { generateFilters } from './mock/filter';
 import { renderComponent } from './utils/render';
+
+const tasksModel = new TasksModel();
+tasksModel.setTasks(tasks);
 
 const $siteMain = document.querySelector(`.js-main`);
 const $siteMainControl = $siteMain.querySelector(`.js-main__control`);
