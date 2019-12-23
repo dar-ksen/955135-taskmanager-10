c37d3fa74498bb8eae7670110015ff355cf30535
Dar <ksen.vl.set@gmail.com>
2019-12-23 19:59:19 +0300

    Add check on Overdue date in common.js

--- src/utils/common.js
+++ src/utils/common.js
@@ -10,11 +10,23 @@ const isRepeating = (repeatingDays) => Object.values(repeatingDays).some(Boolean
 
 const replace = (collection, replacement, index) => [...collection.slice(0, index), replacement, ...collection.slice(index + 1)];
 
+const isOverdueDate = (dueDate, date) => {
+  return dueDate < date && !isOneDay(date, dueDate);
+};
+
+const isOneDay = (dateA, dateB) => {
+  const a = moment(dateA);
+  const b = moment(dateB);
+  return a.diff(b, `days`) === 0 && dateA.getDate() === dateB.getDate();
+};
+
 export {
   formatTime,
   formatDate,
   isFirst,
   take,
   isRepeating,
-  replace
+  replace,
+  isOneDay,
+  isOverdueDate
 };
