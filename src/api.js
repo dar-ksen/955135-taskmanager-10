91a2c6c78f16cdc9c3d21b43fc4e599193364150
Dar <ksen.vl.set@gmail.com>
2020-01-07 10:02:43 +0300

    Describes a helper function for checking response status

--- src/api.js
+++ src/api.js
@@ -4,6 +4,15 @@ const METHOD = {
   PUT: `PUT`,
   DELETE: `DELETE`
 };
+
+const checkStatus = (response) => {
+  if (response.status >= 200 && response.status < 300) {
+    return response;
+  } else {
+    throw new Error(`${response.status}: ${response.statusText}`);
+  }
+};
+
 const API = class {
 
 };
